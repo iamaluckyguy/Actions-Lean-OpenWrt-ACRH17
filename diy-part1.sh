fd195eddd566e1851c9cd687c8fcc98027971e9f
iamaluckyguy <64719828+iamaluckyguy@users.noreply.github.com>
2020-05-10 00:30:48 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Lienol/openwrt-package
+#sed -i '$a src-git lienol https://github.com/TinyTitanPro/lienol-openwrt-package.git' feeds.conf.default
+
+sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
+
