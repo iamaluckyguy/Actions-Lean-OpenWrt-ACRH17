a412af51343c6f21816052eccf1807a8602326d2
iamaluckyguy <64719828+iamaluckyguy@users.noreply.github.com>
2020-05-11 00:16:46 +0800

    Update diy-part3-uploadconfig.sh

--- diy-part3-uploadconfig.sh
+++ diy-part3-uploadconfig.sh
@@ -1,4 +1,7 @@
+  
 #!/bin/sh
+
+# This is not in use any longer as the seed.config is cofigured to upload to WeTransfer and CowTransfer in the main file.
 
 #upload config to ftp after menuconfig
 
@@ -7,13 +10,13 @@
 # cd openwrt is not necessary as it's config in RUN to cd openwrt
 #cd openwrt
 date=`date +%Y-%m-%d-%H-%M-%S`
-newfilename=seed.config-ACRH17-$date
+newfilename=seed.config-newwifi3-$date
 
 cp seed.config $newfilename
 
 PUTFILE=$newfilename
-ftp -v -n 34.92.60.168<<EOF
-user Config ConfigUpload
+ftp -v -n 252.188.136.8<<EOF
+user FTPUser FTPPass
 passive
 put $PUTFILE
 bye
